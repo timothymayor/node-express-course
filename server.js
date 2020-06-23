04de709c455a1d476fe2368c29430e9a1c533017
timothymayor <timothymayor24@gmail.com>
2020-06-23 21:28:13 +0100

    add login POST route

--- server.js
+++ server.js
@@ -1,17 +1,21 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
 
 const mockUserData=[
     {name:'Mark'},
     {name:'Jill'}
 ]
-// app.get('/users', function(req,res){
-//     res.json({
-//         success: true,
-//         message: 'successfully got users. Nice!',
-//         users: mockUserData
-//     })
-// })
+
+app.get('/users', function(req,res){
+    res.json({
+        success: true,
+        message: 'successfully got users. Nice!',
+        users: mockUserData
+    })
+})
 
 app.get('/users/:id',function(req,res){
     console.log(req.params.id)
@@ -20,6 +24,30 @@ app.get('/users/:id',function(req,res){
         message: 'got one user',
         user: req.params.id
     })
+})
+
+app.post('/login',function(req.res){
+    //Typically passwords are encryted using something like bcrypt before sending to database
+    const username=req.body.username;
+    const password=req.body.password;
+
+    //This should come from the database
+    const mockUsername="billyTheKid";
+    const mockPassword="superSecret";
+
+    if (username===mockUsername && password===mockPassword){
+        //In practice, use JSON web token sign method here to make an encrypted token
+        res.json({
+            success: true,
+            message: 'password and username match!',
+            token: 'encrypted token goes here'
+        })
+    } else {
+        res.json({
+            success: false,
+            message: 'password and username do not match!'
+        })
+    }
 })
 
 
